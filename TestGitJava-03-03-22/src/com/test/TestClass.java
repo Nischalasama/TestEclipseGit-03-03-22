38b06482a408480e5f4aee67e30f35e8ccd97e83
Nischalasama <samanischala@gmail.com>
2022-03-04 00:49:52 +0530

    First commit for the main project

---
+++ TestGitJava-03-03-22/src/com/test/TestClass.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World - 01");
+		System.out.println("Eclipse test git");
+
+	}
+
+}
